ff29d1da3d55f8c27768697e7ef8686883240bc7
DealDOne <vladijsaf@mail.ru>
2023-10-28 19:05:18 +0300

    Initial commit

---
+++ src/test/java/org/example/geo/GeoServiceImplTest.java
@@ -0,0 +1,72 @@
+package org.example.geo;
+
+import org.example.entity.Country;
+import org.example.entity.Location;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoServiceImplTest {
+
+    static class LocationPlusIp{
+        static Location location;
+        static String ip;
+
+        public LocationPlusIp(Location location, String ip){
+            this.location = location;
+            this.ip = ip;
+        }
+        public Location getLocation() {
+            return location;
+        }
+        public String getIp() {
+            return ip;
+        }
+    };
+
+    @ParameterizedTest
+    @MethodSource
+    void testWithMethodSource(LocationPlusIp argument) {
+
+        Location location = argument.getLocation();
+        String ip = argument.getIp();
+
+        GeoService geoService = new GeoServiceImpl();
+        Assertions.assertEquals(location.getCity(), geoService.byIp(ip).getCity());
+        Assertions.assertEquals(location.getCountry(), geoService.byIp(ip).getCountry());
+        Assertions.assertEquals(location.getStreet(), geoService.byIp(ip).getStreet());
+        Assertions.assertEquals(location.getBuiling(), geoService.byIp(ip).getBuiling());
+    }
+
+    static Stream<LocationPlusIp> testWithMethodSource() {
+        return Stream.of(new LocationPlusIp(new Location("Moscow", Country.RUSSIA, "Lenina", 15), "172.0.32.11"),
+                new LocationPlusIp(new Location("New York", Country.USA, "10th Avenue", 32), "96.44.183.149"),
+                new LocationPlusIp(new Location(null, null, null, 0), "127.0.0.1"));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(LocationsArgumentsProvider.class)
+    void testByIpArgumentsSource(Location location, String ip) {
+        GeoService geoService = new GeoServiceImpl();
+        Assertions.assertEquals(location.getCity(), geoService.byIp(ip).getCity());
+        Assertions.assertEquals(location.getCountry(), geoService.byIp(ip).getCountry());
+        Assertions.assertEquals(location.getStreet(), geoService.byIp(ip).getStreet());
+        Assertions.assertEquals(location.getBuiling(), geoService.byIp(ip).getBuiling());
+    }
+    static class LocationsArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of(new Location("Moscow", Country.RUSSIA, "Lenina", 15), "172.0.32.11"),
+                    Arguments.of(new Location("New York", Country.USA, "10th Avenue", 32), "96.44.183.149"),
+                    Arguments.of(new Location(null, null, null, 0), "127.0.0.1")
+            );
+        }
+    }
+}
---
+++ src/test/java/org/example/i18n/LocalizationServiceImplTest.java
@@ -0,0 +1,43 @@
+package org.example.i18n;
+
+import org.example.entity.Country;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalizationServiceImplTest {
+
+    @ParameterizedTest
+    @ArgumentsSource(CountryArgumentsProvider.class)
+    void testLocale(Country country) {
+        LocalizationService localizationService = new LocalizationServiceImpl();
+        if (country.equals(Country.RUSSIA)){
+            String expectedValue = "Добро пожаловать";
+            Assertions.assertEquals(expectedValue, localizationService.locale(country));
+        } else {
+            String expectedValue = "Welcome";
+            Assertions.assertEquals(expectedValue, localizationService.locale(country));
+        }
+    }
+
+    static class CountryArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of(Country.RUSSIA),
+                    Arguments.of(Country.GERMANY),
+                    Arguments.of(Country.USA),
+                    Arguments.of(Country.BRAZIL)
+            );
+        }
+    }
+}
